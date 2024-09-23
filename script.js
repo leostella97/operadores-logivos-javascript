51e526c81c1b14d6bc77baa115002e15bb780332
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2024-09-23 10:13:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// %% representa AND (e)
+// || representa OR (ou)
+// ! representa NOT (não)
+
+let num1 = 10
+let num2 = 2
+
+// AND
+if (num1 == 10 && num2 == 2){
+    console.log ("num1 é igual a " + num1 + " e num2 é igual a " + num2)
+}
+
+// OR
+if (num1 == 10 || num2 == 2){
+    console.log ("num1 é igual a " + num1 + " ou num2 é igual a " + num2)
+}
+
+// NOT
+if (!(num1 == 1)){
+    console.log ("num1 não é igual a " + num1)
+}
+
