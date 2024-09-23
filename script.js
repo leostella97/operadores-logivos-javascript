3a5aebff5dc809a1604798cbf634eb2f9961af75
Leonardo Stella de Oliveira <leonardo@lesttech.com.br>
2024-09-23 10:13:39 -0300

    allow-empty

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ let num1 = 10
 let num2 = 2
 
 // AND
-if (num1 == 10 && num2 == 2){
+let operAND = num1 == 10 && num2 == 2
+if (operAND){
     console.log ("num1 é igual a " + num1 + " e num2 é igual a " + num2)
 }
 
 // OR
-if (num1 == 10 || num2 == 2){
+let operOR = num1 == 10 || num2 == 2
+if (operOR){
     console.log ("num1 é igual a " + num1 + " ou num2 é igual a " + num2)
 }
 
 // NOT
-if (!(num1 == 1)){
+let operNOT = !(num1 == 1)
+if (operNOT){
     console.log ("num1 não é igual a " + num1)
 }
 
